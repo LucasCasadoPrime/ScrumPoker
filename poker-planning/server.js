daf0105b4328aa722d435eff6430c4352d1c44af
Lucas Casado <lucas.casado@epitech.eu>
2025-02-13 17:22:46 +0100

    cors

--- poker-planning/server.js
+++ poker-planning/server.js
@@ -8,6 +8,12 @@ const handle = app.getRequestHandler();
 
 const rooms = {};
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.prepare().then(() => {
     const server = http.createServer((req, res) => handle(req, res));
     const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
