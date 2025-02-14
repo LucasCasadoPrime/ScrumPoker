466dc5f123022197b4cf42b9d88ac18f54e23bb8
Lucas Casado <lucas.casado@epitech.eu>
2025-02-14 09:49:02 +0100

    cors reparer

--- poker-planning/server.js
+++ poker-planning/server.js
@@ -8,7 +8,7 @@ const handle = app.getRequestHandler();
 
 const rooms = {};
 
-app.use((req, res, next) => {
+app.prepare((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
